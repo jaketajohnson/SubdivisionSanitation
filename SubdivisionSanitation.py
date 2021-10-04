37b95a0bfa25e4bd5921477bc87dc6ab9adc9975
Jake Johnson <56829096+jaketajohnson@users.noreply.github.com>
2021-10-04 10:30:44 -0500

    Logging abstraction

--- SubdivisionSanitation.py
+++ SubdivisionSanitation.py
@@ -5,9 +5,7 @@
 
  DESCRIPTION
 
-    * Attributes the spatial and facility identifiers for the sewer and stormwater tables
-    * Calculates the GXPCity field for sewer gravity mains by finding only gravity mains in that ward polygon
-    * Appends new GPS points from the Y: drive and calculates a spatial identifier
+    * Sanitizes subdivision names (replaces apostrophes with empty spaces)
 
  REQUIREMENTS
 
@@ -16,40 +14,15 @@
  """
 
 import arcpy
-import logging
 import os
-import sys
 import traceback
+import sys
+sys.path.insert(0, "C:/Scripts")
+import Logging
 
 
-def ScriptLogging():
-    """Enables console and log file logging; see test script for comments on functionality"""
-    current_directory = os.getcwd()
-    script_filename = os.path.basename(sys.argv[0])
-    log_filename = os.path.splitext(script_filename)[0]
-    log_file = os.path.join(current_directory, f"{log_filename}.log")
-    if not os.path.exists(log_file):
-        with open(log_file, "w"):
-            pass
-    message_formatting = "%(asctime)s - %(levelname)s - %(message)s"
-    date_formatting = "%Y-%m-%d %H:%M:%S"
-    formatter = logging.Formatter(fmt=message_formatting, datefmt=date_formatting)
-    logging_output = logging.getLogger(f"{log_filename}")
-    logging_output.setLevel(logging.INFO)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    logging_output.addHandler(console_handler)
-    logging.basicConfig(format=message_formatting, datefmt=date_formatting, filename=log_file, filemode="w", level=logging.INFO)
-    return logging_output
-
-
-def SubdivisionSanitation():
-    """Collection of attribution functions"""
-
-    # Logging
-    logger = ScriptLogging()
-    logger.info("Script Execution Start")
+def subdivision_sanitation():
+    """Sanitize subdivision names"""
 
     # Environment settings
     arcpy.env.overwriteOutput = True
@@ -58,33 +31,22 @@ def SubdivisionSanitation():
     parcel_test = r"F:\Shares\FGDB_NonCityData\SANGIS_Data.gdb"
     subdivision_polygons = os.path.join(parcel_test, "Sub_Poly")
 
-    # Try running the simple function below
+    # Replace apostrophes
+    expression = "!Sub_Name!.replace(\"\'\", \"\")"
+    arcpy.CalculateField_management(subdivision_polygons, "Sub_Name", expression, "PYTHON3")
+
+
+if __name__ == "__main__":
+    traceback_info = traceback.format_exc()
     try:
-        expression = "!Sub_Name!.replace(\"\'\", \"\")"
-        arcpy.CalculateField_management(subdivision_polygons, "Sub_Name", expression, "PYTHON3")
-    except (IOError, KeyError, NameError, IndexError, TypeError, UnboundLocalError, ValueError):
-        traceback_info = traceback.format_exc()
-        try:
-            logger.info(traceback_info)
-        except NameError:
-            print(traceback_info)
+        Logging.logger.info("Script Execution Started")
+        subdivision_sanitation()
+        Logging.logger.info("Script Execution Finished")
+    except (IOError, NameError, KeyError, IndexError, TypeError, UnboundLocalError, ValueError):
+        Logging.logger.info(traceback_info)
+    except NameError:
+        print(traceback_info)
     except arcpy.ExecuteError:
-        try:
-            logger.error(arcpy.GetMessages(2))
-        except NameError:
-            print(arcpy.GetMessages(2))
+        Logging.logger.error(arcpy.GetMessages(2))
     except:
-        logger.exception("Picked up an exception!")
-    finally:
-        try:
-            logger.info("Script Execution Complete")
-        except NameError:
-            pass
-
-
-def main():
-    SubdivisionSanitation()
-
-
-if __name__ == '__main__':
-    main()
+        Logging.logger.info("An unspecified exception occurred")
